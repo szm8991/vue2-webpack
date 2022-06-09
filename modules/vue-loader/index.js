c083518a106d32032fd34c81df68d0d9cc89e403
zm <68761971+szm8991@users.noreply.github.com>
2022-06-09 09:12:25 +0800

    chore: add .vue compiler

--- modules/vue-loader/index.js
+++ modules/vue-loader/index.js
@@ -1,4 +1,9 @@
+const regTemplate = /\<template\>([\s\S]+?)<\/template\>/
+const regScript = /\<script\>([\s\S]+?)<\/script\>/
+const regReplace = /({)/
 module.exports = function (source) {
-  //   console.log(source)
-  return '123'
+  const template = source.match(regTemplate)[1].trim()
+  const script = source.match(regScript)[1].trim()
+  const finalScript = script.replace(regReplace, '$1 template:' + '`' + template + '`' + ',')
+  return finalScript
 }
